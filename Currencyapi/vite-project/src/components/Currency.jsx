078f4d64f3e0a2f2d584547578789a5957f0ebd5
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-07-04 16:44:38 +0300

    Fifth second

--- Currencyapi/vite-project/src/components/Currency.jsx
+++ Currencyapi/vite-project/src/components/Currency.jsx
@@ -1,98 +1,72 @@
-import React, { useState } from "react";
-import { FaArrowRight } from "react-icons/fa6";
-import "../css/Currency.css";
-import axios from "axios";
-import { useEffect } from "react";
+import React, { useState } from 'react';
+import '../css/Currency.css';
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import axios from 'axios';
+
+let BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+let API_KEY = "fca_live_0dle8NOODwNTkD5i4EFlF4hKx3Yrpqcl4PrbHk0u";
 
 function Currency() {
-    const [amount, setAmount] = useState(0);
-    const [fromCurrency, setFromCurrency] = useState("USD");
-    const [toCurrency, setToCurrency] = useState("TRY");
-    const [result, setResult] = useState("");
-
-    let token = "fca_live_FXhn90pUyFbfVTdrOWM2D7FM3b9wcfnXrzIYN0tO";
-    let baseUrl = "https://api.freecurrencyapi.com/v1/latest";
+    const [amount, setAmount] = useState('');
+    const [fromCurrency, setFromCurrency] = useState('USD');
+    const [toCurrency, setToCurrency] = useState('TRY');
+    const [result, setResult] = useState(0);
 
     const exchange = async () => {
         try {
-            const response = await axios.get(
-                `${baseUrl}?apikey=${token}&base_currency=${fromCurrency}`
-            );
-            const result = (response.data.data[toCurrency] * amount).toFixed(2);
-            setResult(result);
+            const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
+            const rate = response.data.data[toCurrency];
+            const calculatedResult = (rate * amount).toFixed(2);
+            setResult(calculatedResult);
         } catch (error) {
-            console.log("hata olustu ", error);
+            console.error("Hata oluştu:", error);
         }
     };
 
     return (
-        <div>
-            <div className="currency-main">
-                <div
-                    style={{
-                        backgroundColor: "#5E5D5D",
-                        width: "100%",
-                        textAlign: "center",
-                        borderRadius: "5px",
-                    }}
-                >
-                    <h3 style={{ fontFamily: "arial", color: "#fff" }}>
-                        DÖVİZ KURU UYGULAMASI
-                    </h3>
-                </div>
-
-                <div style={{ marginTop: "20px" }}>
-                    <input
-                        id="amount"
-                        type="number"
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                        className="amount-input"
-                    />
-                    <select
-                        onChange={(e) => setFromCurrency(e.target.value)}
-                        className="first-currency-option"
-                    >
-                        <option>USD</option>
-                        <option>EUR</option>
-                        <option>TRY</option>
-                    </select>
-                    <FaArrowRight style={{ marginRight: "10px" }} />
-
-                    <select
-                        onChange={(e) => setToCurrency(e.target.value)}
-                        className="second-currency-option"
-                    >
-                        <option>TRY</option>
-                        <option>USD</option>
-                        <option>EUR</option>
-                    </select>
-
-                    <input
-                        value={result}
-                        onChange={(e) => setResult(e.target.value)}
-                        type="text"
-                        className="result-input"
-                    />
-                </div>
-
-                <div className="button-main">
-                    <button
-                        style={{
-                            width: "100px",
-                            height: "30px",
-                            border: "none",
-                            backgroundColor: "green",
-                            color: "#fff",
-                            cursor: "pointer",
-                            borderRadius: "5px",
-                            marginTop: "20px",
-                        }}
-                        onClick={exchange}
-                    >
-                        Çevir
-                    </button>
-                </div>
+        <div className='currency-div'>
+            <div style={{
+                fontFamily: 'arial',
+                backgroundColor: 'black',
+                color: '#fff',
+                width: '100%',
+                textAlign: 'center'
+            }}>
+                <h3>Döviz Kuru Uygulaması</h3>
+            </div>
+            <div style={{ marginTop: '25px' }}>
+                <input
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    type="number"
+                    className='amount'
+                />
+                <select
+                    onChange={(e) => setFromCurrency(e.target.value)}
+                    className='from-currency-option'>
+                    <option>USD</option>
+                    <option>EUR</option>
+                    <option>TRY</option>
+                </select>
+                <FaArrowAltCircleRight
+                    style={{ marginTop: '5px', fontSize: '25px', color: 'black', marginRight: '10px' }}
+                />
+                <select
+                    onChange={(e) => setToCurrency(e.target.value)}
+                    className='to-currency-option'>
+                    <option>TRY</option>
+                    <option>USD</option>
+                    <option>EUR</option>
+                </select>
+                <input
+                    value={result}
+                    readOnly
+                    type="number"
+                    className='result'
+                />
+            </div>
+            <div>
+                <button onClick={exchange} className='exchange-button'>Çevir</button>
             </div>
         </div>
     );
